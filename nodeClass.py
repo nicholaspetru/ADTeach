d2ba3e743d0111e805e7b58d1e5b2908725c8d50
Emily Shack <shacke@carleton.edu>
2014-10-03 19:42:32 -0500

    updated nodeClass

--- nodeClass.py
+++ nodeClass.py
@@ -4,51 +4,64 @@ class Node:
     Next = None
     def __init__(self, token):
         self.Token = token
+        self.Type = self.setType()
+    def setNext(self, Node):
+        self.Next = Node
+    def setType(self):
+        '''
+        0 = error
+        1 = symbol
+        2 = integer
+        3 = double
+        4 = string
+        5 = open paren
+        6 = close paren
+        7 = true
+        8 = false
+        '''
         period = 0
         index = 0
-        if(token[0] == '\"' and token[len(token)-1] == '\"'):
-            self.Type = 4
-        elif (token[0] == '('):
-            self.Type = 5
-        elif (token[0]  == ')'):
-            self.Type = 6
-        elif (token[0] == '#'):
-            if (token[1] == 't'):
-                self.Type = 7
-            elif (token[1]  == 'f'):
-                self.Type = 8
+        if(self.Token[0] == '\"' and self.Token[len(self.Token)-1] == '\"'):
+            return 4
+        elif (self.Token[0] == '('):
+            return 5
+        elif (self.Token[0]  == ')'):
+            return 6
+        elif (self.Token[0] == '#'):
+            if (self.Token[1] == 't'):
+                return 7
+            elif (self.Token[1]  == 'f'):
+                return 8
             else:
-                self.Type = 0
-        elif (token[0]  == '\''):
-            self.Type = 1
-        if (token[0]  == '-'):
-            if (len(token) == 1):
-                self.Type = 1
+                return 0
+        elif (self.Token[0]  == '\''):
+            return 1
+        if (self.Token[0]  == '-'):
+            if (len(self.Token) == 1):
+                return 1
             else:
                 index = 1
-        for i in range(index, len(token)):
+        for i in range(index, len(self.Token)):
             # If it is a digit, using ASCII values
-            if( ord(token[i]) >= 48 and ord(token[i]) <= 57):
+            if( ord(self.Token[i]) >= 48 and ord(self.Token[i]) <= 57):
                 continue
             # if it is a period
-            elif(token[i] == '.'):
+            elif(self.Token[i] == '.'):
                 if (period == 1):
-                    self.Type = 0
+                    return 0
                 else:
                     period = 1
             # if it is a character or any other symbol
             else:
                 # invalid input
                 if(i == 0):
-                    self.Type = 1
+                    return 1
                 else:
-                    self.Type = 0
+                    return 0
         if (period == 0):
-            self.Type = 2 #Its an integer
+            return 2
         else:
-            self.Type = 3 # Its a double
-    def setNext(self, Node):
-        self.Next = Node
+            return 3 
     def getToken(self):
         return self.Token
     def getType(self):
@@ -56,10 +69,11 @@ class Node:
     def getNext(self):
         return self.Next
     
+    
 if __name__ == '__main__':
-    node1 = Node("'hello'")
+    node1 = Node('"hello"')
     print node1.getToken()
     print node1.getType()
-    node2 = Node("'goodbye'")
+    node2 = Node('"goodbye"')
     node1.setNext(node2)
     print node1.getNext()
