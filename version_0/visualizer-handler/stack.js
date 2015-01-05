2276e0bc53dbdb01545e3a540629297e23b1bf1b
nicholaspetru <petrun@carleton.edu>
2015-01-04 18:18:35 -0600

    Working on stack.js. Still need to make a couple changes and work it into the vis handler

--- version_0/visualizer-handler/stack.js
+++ version_0/visualizer-handler/stack.js
@@ -1,19 +1,56 @@
 //stack.js
 //A test implementation of a stack.
 
+/*
+Put this code in  Redner() in visualizer-handler.js to test this code
+
+var xstack = new Stack(paper, "test stack", "Integer", ["test", "a", "is", "this"]);
+        xstack.Draw(300, 200);
+        xstack.Populate();
+*/
+
 $(document).ready(function () {
     
-    Stack = function(paper,name,type,value){
-	Entity.call(this,paper,name,type,value);
-	//specific calls go here
-	this.type = "stack<" + type + ">"; 
+    // eventually we will want to pass in x, y, width, and height into Stack instatiations
+    // currently the Draw function (in primitives) takes x and y
+    Stack = function(paper,name,type,value) /*, x, y, width, height)*/{
+        //assigning the stack attributes
+        this.paper = paper;
+        this.name = name;
+        this.type = "stack<" + type + ">"; 
+        this.value = value;
+        this.x = 250;
+        this.y = 250;
+        this.width = 40;
+        this.height = 200;
     }
     
     Stack.prototype = Object.create(Entity.prototype);
     Stack.prototype.constructor = Stack;
 
-    //draw the name of the function
-    Stack.prototype.Draw = function(x, y){
-	   console.log(this.DrawName() + " = " + this.value);
+    //draw a Stack of the function
+    Stack.prototype.Draw = function(){
+        var box = paper.path("M " + this.x + ", " + this.y + " V " + (this.y + this.height) + " H " + (this.x + this.width) + " V " + this.y);
+        var name = paper.text(this.x + this.width/2, this.y + this.height + 10, this.name)
+        console.log(this.DrawName() + " = " + this.value);
     }
+
+    // Loop through values and put them into the stack
+    Stack.prototype.Populate = function(){
+        var tempY;
+        for (var i = 0; i < this.value.length; i++) {
+            var item = paper.rect(this.x + 2, (this.y + this.height) - (i+1)*(this.width), this.width - 4, this.width - 4);
+            
+            if (i == 0) 
+                tempY = (this.y + this.height) - (i+1)*(this.width/2 + 4);
+            else 
+                tempY = tempY - (this.width);
+
+            var val = paper.text(this.x + this.width/2, tempY, this.value[i]);
+        }
+    }
+
 });
+
+
+
