8859a9ad4fe940d6f26740c1ae12e54f20833a6d
Emily Shack <shacke@carleton.edu>
2015-01-13 18:02:45 +0000

    Code Database working!

--- version_0/code-db/code-database.js
+++ version_0/code-db/code-database.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
-    CodeDatabase = function(database) {
-        //the list of entities
-        this.database = database;
+    CodeDatabase = function() {
+        //hash table of code samples
+        this.database = new Object();
+        this.database['stack'] = 
+            ['int x = 5;',
+             'Stack<Integer> s = new Stack<Integer>();',
+             's.push(x);\ns.pop();',
+            ].join('\n');
+        this.database['stack2'] = 
+            ['Stack<String> s = new Stack<String>();',
+            's.push("hello")',
+            's.push("world");',
+            ].join('\n');
         return this;
     }
-    
+
     CodeDatabase.prototype.getCode = function(key) {
         //given some key, returns the associated value in the database
-        //if we want database to be a hash table: 
-        //(write our own) http://www.mojavelinux.com/articles/javascript_hashes.html
-        //(use a library) http://www.timdown.co.uk/jshashtable/
         console.log('Code Database: getCode(' + key + ')');
-        var code = 'int x = 5;\nStack<Integer> s = new Stack<Integer>();\ns.push(x);\ns.pop();'
+        var code = this.database[key];
         return code;
     };
     
