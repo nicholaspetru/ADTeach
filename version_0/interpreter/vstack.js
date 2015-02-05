3a3c52d26b0068064d006d5f40a95c17e3a22423
Sarah <sleblanc0218@yahoo.com>
2015-02-04 18:38:43 -0600

    Finished stack methods

--- version_0/interpreter/vstack.js
+++ version_0/interpreter/vstack.js
@@ -29,19 +29,20 @@ $(document).ready(function () {
     }
     
     VStack.prototype.listMethods = function() {
-        var methods = ['getValue', 'isEmpty', 'push', 'pop', 'populate'];
+        var methods = ['getValue', 'isEmpty', 'push', 'pop', 'populate', 'peek', 'search'];
         return methods;
     }
     
     VStack.prototype.checkParameters = function(method, parameters) {
-        var noParam = ['pop', 'getValue', 'isEmpty'];
+        var noParam = ['pop', 'getValue', 'isEmpty', 'peek'];
+        var oneParam = ['push', 'populate', 'search'];
         if (noParam.indexOf(method) >= 0) {
             if (parameters.length != 0) {
                 console.log("no parameters");
                 //new IncorrectParameters();
             }
         }
-        if (method == "push" || method == "populate") {
+        if (oneParam.indexOf(method) >= 0) {
             if (parameters.length != 1) {
                 console.log("need one parameter");
                 //new IncorrectParameters();
@@ -56,10 +57,30 @@ $(document).ready(function () {
         for (var i = 0; i<origValue1.length; i++){
             origValue[i]=(origValue1[i]);   
         }
+        
         if (method == "pop") {
             returnValue = origValue.pop();
             return [returnValue, origValue];
-        } if (method == "push") {
+        } 
+        
+        if (method == "search") {
+            var count = 1;
+            if (origValue.indexOf(parameters[0].value) >= 0) {
+                for (var i = origValue.length - 1; i > -1; i--) {
+                    if (origValue[i] == parameters[0].value) {
+                        returnValue = count;
+                        break;
+                    } else {
+                        count += 1;
+                    }
+                }
+            } else {
+                returnValue = -1;
+            }
+            return [returnValue, origValue];
+        }
+        
+        if (method == "push") {
             if (type == "Stack<Integer>") {
                 if (parameters[0].jtype != "INT_TYPE") {
                     console.log('INCOMPATIBLE TYPES, want ints');
@@ -72,13 +93,26 @@ $(document).ready(function () {
             //TODO: Check types
             origValue.push(parameters[0].value);
             return [returnValue, origValue];
-        } if (method == "isEmpty") {
-            return origValue.isEmpty();
-        } if (method == "getValue") {
+        } 
+        
+        if (method == "isEmpty") {
+            if (origValue.length == 0) {
+                returnValue = true;
+            } else {
+                returnValue = false;
+            }
+            return [returnValue, origValue];
+        } 
+        
+        if (method == "getValue") {
             return origValue.getValue();
-        } if (method == "getType") {
+        } 
+        
+        if (method == "getType") {
             return origValue.getType();
-        } if (method == "populate") {
+        } 
+        
+        if (method == "populate") {
             if (type == "Stack<Integer>") {
                 var value = [];
                 for (i = 0; i < parameters[0].value; i++) {
@@ -96,6 +130,12 @@ $(document).ready(function () {
                 }
                 return [returnValue, value];
             }
+        }
+        
+        if (method == "peek") {
+            var length = origValue.length;
+            returnValue = origValue[length-1];
+            return [returnValue, origValue];
         }
         
     }
