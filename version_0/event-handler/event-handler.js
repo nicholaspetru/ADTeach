df4aeea0fcb075649671d22b4be48f592e988f21
Emily Shack <shacke@carleton.edu>
2015-02-15 23:05:07 +0000

    highlightLine fix 1/5

--- version_0/event-handler/event-handler.js
+++ version_0/event-handler/event-handler.js
@@ -67,6 +67,7 @@ $(document).ready(function() {
        console.log("Event Handler: onStop()");
        this.codeBox.unfreezeCode();
        this.visualizerHandler.DeleteAll();
+       $("#highlight_paper").hide();
         //show the needed buttonz
         $("#play").hide();
         $("#pause").hide();
